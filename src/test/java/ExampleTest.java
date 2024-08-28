9460b13aa4f6e081be5f54f08b1405f0347a7e93
SergeyEroshkin <petyshki@yandex.ru>
2024-08-28 21:17:54 +0300

    init commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void addition() {
+
+        assertEquals(2, 2);
+    }
+}
